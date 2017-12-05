3fe669a4ada445af53514f5796eaa0f6b94c2d3f
Huming Tang <tangh02@med.nyu.edu>
2017-12-04 21:04:54 -0500

    updated data encoder error

--- src/AppBundle/Controller/SyncController.php
+++ src/AppBundle/Controller/SyncController.php
@@ -13,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Utils\Validator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Utils\DataEncoder;
 
 class SyncController extends Controller
 {
